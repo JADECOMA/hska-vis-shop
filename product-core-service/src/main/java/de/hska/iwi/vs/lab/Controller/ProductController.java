b7f5ee9349c2f4c74a763497fbed834c3e82ae53
Manuel Härtle <info@jadecoma.de>
2018-05-24 11:28:57 +0200

    Adding service methods

--- product-core-service/src/main/java/de/hska/iwi/vs/lab/Controller/ProductController.java
+++ product-core-service/src/main/java/de/hska/iwi/vs/lab/Controller/ProductController.java
@@ -1,8 +1,10 @@
 package de.hska.iwi.vs.lab.Controller;
 
+import de.hska.iwi.vs.lab.Entity.Product;
 import de.hska.iwi.vs.lab.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,24 +14,30 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping()
-    @ResponseBody
-    public HttpStatus testIt() {
-        return HttpStatus.OK;
-    }
-
-    @GetMapping("/search/{searchPhrase}")
+    @GetMapping(value = "/search/{searchPhrase}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> searchProducts(@PathVariable String searchPhrase) {
         return new ResponseEntity<>(productService.searchProducts(searchPhrase), HttpStatus.OK);
     }
 
-    @GetMapping("/view/{productId}")
+    @GetMapping(value = "/view/{productId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> findById(@PathVariable int productId) {
         return new ResponseEntity<>(productService.findById(productId), HttpStatus.OK);
     }
 
-    @RequestMapping("/test/{testIt}")
-    public String ok(@PathVariable String testIt) {
-        return testIt;
+    @DeleteMapping(value = "/{productId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> deleteProduct(@PathVariable int productId) {
+        HttpStatus status = productService.deleteProduct(productId);
+        return new ResponseEntity<>(status);
+    }
+
+    @PutMapping()
+    public ResponseEntity<?> addProduct(@RequestBody Product product) {
+        HttpStatus status = productService.addProduct(product);
+        return new ResponseEntity<>(status);
+    }
+
+    @GetMapping(value = "/test/{testIt}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> ok(@PathVariable String testIt) {
+        return new ResponseEntity<>(testIt, HttpStatus.OK);
     }
 }
--- product-core-service/src/main/java/de/hska/iwi/vs/lab/Service/ProductService.java
+++ product-core-service/src/main/java/de/hska/iwi/vs/lab/Service/ProductService.java
@@ -3,11 +3,13 @@ package de.hska.iwi.vs.lab.Service;
 import de.hska.iwi.vs.lab.Entity.Product;
 import de.hska.iwi.vs.lab.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.function.Predicate;
 
-@Component
+@Service
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
@@ -20,18 +22,67 @@ public class ProductService {
             price = Double.parseDouble(searchPhrase);
             minPrice = price - 0.5;
             maxPrice = price + 0.5;
-        } catch (Exception e){}
+        } catch (Exception e) {
+        }
 
         int category_id = 0;
         try {
             category_id = Integer.parseInt(searchPhrase);
-        } catch (Exception e){}
+        } catch (Exception e) {
+        }
 
         return productRepository.findDistinctProductByNameContainingOrDetailsContainingOrPriceBetweenOrCategoryIdAllIgnoreCase(searchPhrase, searchPhrase, minPrice, maxPrice, category_id);
     }
 
     public Product findById(int productId) {
         return productRepository.findById(productId);
+    }
+
+    public HttpStatus addProduct(Product product) {
+        product.toString();
+        if (validate(product)) {
+            Product newProduct = new Product();
+            newProduct.setName(product.getName());
+            newProduct.setDetails(product.getDetails());
+            newProduct.setPrice(product.getPrice());
+            newProduct.setCategoryId(product.getCategoryId());
+            productRepository.save(newProduct);
+
+            return HttpStatus.CREATED;
+        } else {
+            return HttpStatus.BAD_REQUEST;
+        }
+    }
+
+    public HttpStatus deleteProduct(int productId) {
+        productRepository.deleteById(new Long(productId));
+        return HttpStatus.ACCEPTED;
+    }
+
+    private boolean validate(Product product) {
+        return filterName()
+                .and(filterDetails())
+                .and(filterCategoryId())
+                .and(filterPrice())
+                .and(filterCategoryId())
+                .test(product);
+    }
+
+
+    private Predicate<Product> filterName() {
+        return product -> product.getName().length() > 0;
+    }
+
+    private Predicate<Product> filterDetails() {
+        return product -> product.getDetails().length() > 0;
+    }
+
+    private Predicate<Product> filterPrice() {
+        return product -> product.getPrice() > 0;
+    }
+
+    private Predicate<Product> filterCategoryId() {
+        return product -> product.getCategoryId() > 0;
     }
 
 }
