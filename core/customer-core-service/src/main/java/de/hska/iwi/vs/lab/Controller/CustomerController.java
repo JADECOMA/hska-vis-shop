07bfd208ee55ce8c8f562b167639e248d3a26d46
Manuel Härtle <info@jadecoma.de>
2018-05-25 10:09:13 +0200

    Added logging

--- core/customer-core-service/src/main/java/de/hska/iwi/vs/lab/Controller/CustomerController.java
+++ core/customer-core-service/src/main/java/de/hska/iwi/vs/lab/Controller/CustomerController.java
@@ -3,6 +3,8 @@ package de.hska.iwi.vs.lab.Controller;
 import de.hska.iwi.vs.lab.Entity.Customer;
 import de.hska.iwi.vs.lab.Entity.LoginData;
 import de.hska.iwi.vs.lab.Service.CustomerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,29 +12,39 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-//@RequestMapping("/customers")
 public class CustomerController {
+
+    private static Logger log = LoggerFactory.getLogger(CustomerController.class);
+
     @Autowired
     private CustomerService customerService;
 
     @PutMapping(value="/",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> addCustomer(@RequestBody Customer customer) {
+        log.info("URL-PATH: / | METHOD: PUT");
+
         HttpStatus status = customerService.addCustomer(customer);
         return new ResponseEntity<>(status);
     }
 
     @GetMapping(value="/", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> listCustomer() {
+        log.info("URL-PATH: / | METHOD: GET");
+
         return new ResponseEntity<>(customerService.listCustomer(), HttpStatus.OK);
     }
 
     @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> login(@RequestBody LoginData loginData) {
+        log.info("URL-PATH: /login | METHOD: POST");
+
         return new ResponseEntity<>(customerService.login(loginData), HttpStatus.OK);
     }
 
     @GetMapping(value = "/logout", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> logout() {
+        log.info("URL-PATH: /logout | METHOD: GET");
+
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
