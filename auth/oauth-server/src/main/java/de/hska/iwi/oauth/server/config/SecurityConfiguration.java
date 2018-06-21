bf0e167e851113bdd54b1d1eff7e522040471108
Manuel Härtle <info@jadecoma.de>
2018-06-21 18:18:57 +0200

    Working version with oauth

---
+++ auth/oauth-server/src/main/java/de/hska/iwi/oauth/server/config/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package de.hska.iwi.oauth.server.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+@Order(-20)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    AppConfig appConfig;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //auth
+        //        .inMemoryAuthentication()
+        //        .withUser("user").password("password").roles("USER");
+        auth.jdbcAuthentication().dataSource(appConfig.dataSource());
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .requestMatchers().antMatchers("/login", "/oauth/authorize", "/oauth/confirm_access", "/webjars/**")
+                .and()
+                .formLogin().loginPage("/login").permitAll().failureUrl("/login?error")
+                .and()
+                .authorizeRequests().anyRequest().authenticated();
+    }
+}
